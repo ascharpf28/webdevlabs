1a427a86181cc7538479e5bd9de9aa64bd543393
ascharpf28 <ascharpf@andrew.cmu.edu>
2025-03-18 14:56:19 -0400

    button status changed

--- static/script.js
+++ static/script.js
@@ -111,7 +111,7 @@ function showList(){
     var elem=document.getElementById("list");
     var btn=document.getElementById("btn");
     var listDisplay=window.getComputedStyle(elem).display;
-    var btnDisplay=document.getElementById("btn");
+    var btnDisplay=document.getElementById(btn).display;
     if (listDisplay=="block"){
         elem.style.display="none";
         btnDisplay.style.display="none"
